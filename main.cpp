56341f3fb433761d1b147ec1499bd855336926c0
kukiman <kukiman12345@bk.ru>
2019-12-03 12:23:55 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+long long int Packer::binaryToDecimal(QString n)
+{
+    QString num = n;
+    long long int dec_value = 0;
+
+    // Initializing base value to 1, i.e 2^0
+    long long int base = 1;
+
+    long long int len = num.length();
+    for (int i = len - 1; i >= 0; i--) {
+        if (num[i] == '1')
+            dec_value += base;
+        base = base * 2;
+    }
+
+    return dec_value;
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    long long int c= 4616687675601289216;
+    bitset<64> bi(c);
+    //cout<<sizeof(bi)<<"   \n"<<bi<<"\n";
+    QString str="";
+    for(int i=63;0<=i;i--)
+    {
+        //str+= to_string(bi[i]);
+        str+= QString::number(bi[i]);
+    }
+    qDebug()<<str<<"\n\n";
+
+    //long long int gg = binaryToDecimal(str);
+    //cout<<gg<<"\n"<<c;
+    
+    return a.exec();
+}
