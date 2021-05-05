bc8c129c8cc24baf5e7801fcaf95f4ab6c9f86f2
kukiman <kukiman12345@bk.ru>
2021-05-05 11:44:56 +0300

    New function
    New function, stringToBitArray - converts a string to an array of bits

--- main.cpp
+++ main.cpp
@@ -4,7 +4,29 @@
 #include <bitset>
 using namespace std;
 
-string Packer::strBitToChar(string bit)
+string charToBinary(char ch);
+char binaryToChar(string str);
+
+long long int binaryToDecimal(string str);
+string decimalToBinary(long long int num);
+
+string strBitToString(string bit);
+string stringToBitArray(string str);
+
+int main()
+{
+    string str = "";
+    str = stringToBitArray("apple");
+	
+    std::cout << str << endl;
+
+    cout<< strBitToString(str);
+
+    _getch();
+    return 0;
+}
+
+string strBitToString(string bit)
 {
     string str = "",strCache = "";
     int count = 0;
@@ -27,65 +49,57 @@ string Packer::strBitToChar(string bit)
 
 long long int binaryToDecimal(string str)
 {
-	long long int dec_value = 0;
-	long long int base = 1;
+    long long int dec_value = 0;
+    long long int base = 1;
 
-	long long int len = str.length();
-	for (int i = len - 1; i >= 0; i--) {
-		if (str[i] == '1')
-			dec_value += base;
-		base = base * 2;
-	}
+    long long int len = str.length();
+    for (int i = len - 1; i >= 0; i--) {
+        if (str[i] == '1')
+            dec_value += base;
+        base = base * 2;
+    }
 
-	return dec_value;
+    return dec_value;
 }
 
 string decimalToBinary(long long int num)
 {
-	string str = "";
-	bitset<64> bit(num);
+    string str = "";
+    bitset<64> bit(num);
 
-	str = bit.to_string();
+    str = bit.to_string();
 
-	return str;
+    return str;
 }
 
 char binaryToChar(string str)
 {
-	char ch;
-	bitset<8> bit(binaryToDecimal(str));
+    char ch;
+    bitset<8> bit(binaryToDecimal(str));
 
 
-	ch = (char)bit.to_ulong();
+    ch = (char)bit.to_ulong();
 
-	return ch;
+    return ch;
 }
 
 string charToBinary(char ch)
 {
-	bitset<8> bit(ch);
+    bitset<8> bit(ch);
 
-	return bit.to_string();
+    return bit.to_string();
 }
 
-
-int main()
+string stringToBitArray(string str)
 {
-	char ch = 'h';
-	string str = charToBinary(ch);
-	ch = ' ';
-	ch = binaryToChar(str);
-	cout << ch << "\n";
+    string cache = "";
+    if(str.size() == 0)
+        return nullptr;
+    for(auto i: str)
+    {
+        cache += charToBinary(i);
+    }
 
-	/*int b = 4;
-	string str = "";
-
-	str = decimalToBinary(b);
-	b = 0;
-	b = binaryToDecimal(str);
-	cout << b << "\n";*/
-
-
-	_getch();
-	return 0;
+    return cache;
 }
+
